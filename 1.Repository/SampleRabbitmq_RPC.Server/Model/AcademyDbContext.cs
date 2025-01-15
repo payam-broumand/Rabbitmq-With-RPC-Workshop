c0ddb0a8de89a2afd10892eb5c3558728d5e6904
Pyam <payam.broumandi@gmail.com>
2025-01-15 10:42:14 +0330

    seed data to course data model in OnModelCreating method

--- 1.Repository/SampleRabbitmq_RPC.Server/Model/AcademyDbContext.cs
+++ 1.Repository/SampleRabbitmq_RPC.Server/Model/AcademyDbContext.cs
@@ -26,6 +26,36 @@ namespace SampleRabbitmq_RPC.Repository.Model
 				new Category { Id = 2, Title = "Asp.Net Core" }
 			);
 
+			modelBuilder.Entity<Course>().HasData(
+				new Course
+				{
+					Id = 1,
+					Title = "آموزش برنامه نویسی سی شارپ مقدمانی",
+					Price = 1_400_000,
+					CategoryId = 1
+				},
+				new Course
+				{
+					Id = 2,
+					Title = "آموزش جامع الگوهای طراحی در سی شارپ",
+					Price = 1_900_000,
+					CategoryId = 1
+				},
+				new Course
+				{
+					Id = 3,
+					Title = "دوره آموزشی Asp.Net Core 8",
+					Price = 5_400_000,
+					CategoryId = 2
+				},
+				new Course
+				{
+					Id = 4,
+					Title = "آموزش برنامه نویسی سی شارپ پیشرفته",
+					Price = 2_400_000,
+					CategoryId = 1
+				});
+
 			modelBuilder.Entity<Course>()
 				.HasOne(c => c.Category)
 				.WithMany(g => g.Courses)
--- 1.Repository/SampleRabbitmq_RPC.Server/Model/Course.cs
+++ 1.Repository/SampleRabbitmq_RPC.Server/Model/Course.cs
@@ -6,6 +6,6 @@
 		public int Price { get; set; }
 
 		public int CategoryId { get; set; }
-		public Category Category { get; set; } = new();
+		public Category? Category { get; set; } = new();
 	}
 }
